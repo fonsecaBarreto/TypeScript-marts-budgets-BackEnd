af0f12d90fccd75556b4b85f63fe698db0165bfc
fonsecaBarreto <lucasbfonte@gmail.com>
2021-09-10 12:47:53 -0300

    galo

--- dist/main/routes/tests.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const TestController_1 = require("../../presentation/controllers/TestController");
-const testController = new TestController_1.TestController();
-exports.default = (router) => {
-    router.post('/test', testController.execute());
-};
